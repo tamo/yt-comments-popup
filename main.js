75e91e9dcc447e565df05e55d80bff6fed783cdb
Tamo <ttakah+github@gmail.com>
2022-07-23 16:08:10 +0900

    Add comments

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 const DEBUG = false;
-const CTRL = 17;
-const MAXCOMLEN = 150;
-const POPUPDELAY = 800;
-const MAXWIDTHR = 0.5;
+
+// constants, or settings
+const CTRL = 17; // keycode to stop
+const MAXCOMLEN = 150; // trim comments
+const POPUPDELAY = 800; // 1000 = 1 sec
+const MAXWIDTHR = 0.5; // ratio to screen
 const MAXHEIGHTR = 0.7;
-const OFFSETX = 10;
-const OFFSETY = 10;
+const OFFSETX = 10; // position of popup
+const OFFSETY = 10; // relative to cursor
 const PARAMS = new URLSearchParams({
-	maxResults: 10,
-	order: "relevance",
+	maxResults: 10, // default 20 is too many
+	order: "relevance", // or "time"
+	//moderationStatus: "published", // "heldForReview" or "likelySpam"
+	//searchTerms: "",
 	part: "snippet",
-	textFormat: "plaintext"
+	textFormat: "plaintext" // cannot trim "html" safely
 });
 const TIPSTYLE = {
-	visibility: "hidden",
-	display: "block",
+	visibility: "hidden", // to be made visible later
+	display: "block", // "none" prevents size calculation
 	position: "fixed",
-	left: 0,
+	left: 0, // will be placed near the cursor later
 	top: 0,
 	zIndex: 9999999,
 	backgroundColor: "black",
 	color: "white",
 	boxShadow: "0 0 5px 2px rgba(255,255,255,0.5)"
 };
+
+// global variables
 const cache = {};
 let timeout = undefined;
 let cause = undefined;
@@ -30,11 +36,12 @@ let pressed = false;
 let mouseX = 0;
 let mouseY = 0;
 
+// returns a promise
 function fetchComments(videoId, apiKey) {
 	const url = 'https://www.googleapis.com/youtube/v3/commentThreads'
 			+ '?' + PARAMS + '&videoId=' + videoId + '&key=' + apiKey;
 	return fetch(url).then(response => {
-		if(!response.ok)
+		if(!response.ok) // includes disabled cases
 			return {items: []};
 		return response.json();
 	}).then(json => {
@@ -50,6 +57,7 @@ function fetchComments(videoId, apiKey) {
 			comments = "<p>no comments</p>";
 		return cache[videoId] = comments;
 	});
+	// don't catch errors here because the caller does
 }
 
 function getVideoId(url) {
@@ -62,13 +70,13 @@ function mouseEnterListener(event) {
 	const anchor = event.target;
 	if (anchor.tagName != "A")
 		return;
-	if (anchor.role == "button")
+	if (anchor.role == "button") // e.g. next button on player
 		return;
 
 	const vid = getVideoId(anchor.href);
 	if (!vid)
 		return;
-	if (vid == getVideoId(location.href))
+	if (vid == getVideoId(location.href)) // current video
 		return;
 	if (DEBUG == true)
 		console.log("mouseenter: ", vid);
@@ -78,41 +86,51 @@ function mouseEnterListener(event) {
 			return;
 		const tooltips = anchor.getElementsByTagName("tooltip");
 		if (tooltips.length) {
+			// already has a tooltip
+			// so reuse it by simulating createPopup()
 			hideTips();
 			timeout = setTimeout(function(tooltips){
 				for (let tip of tooltips) {
-					showTips(tip);
+					showTip(tip);
+					break;
 				}
 			}, POPUPDELAY, tooltips);
 			cause = anchor;
 		} else {
+			// this is an anchor without tooltip
+			// but the comments have been cached before
 			createPopup(anchor, cache[vid]);
 		}
 		return;
 	}
-	chrome.storage.local.get("api_key", storage => {
-		const apiKey = storage.api_key;
-		if (!apiKey) {
-			alert("No API key is set.");
-			return;
-		}
-		fetchComments(vid, apiKey).then(comments => {
-			if (pressed)
+	try {
+		chrome.storage.local.get("api_key", storage => {
+			const apiKey = storage.api_key;
+			if (!apiKey) {
+				alert("No API key is set.");
 				return;
-			createPopup(anchor, comments);
-		}).catch(error => {
-			hideTips();
-			if (DEBUG == true)
+			}
+			fetchComments(vid, apiKey).then(comments => {
+				if (pressed)	// return after fetching 
+					return;	// even when pressed
+				createPopup(anchor, comments);
+			}).catch(error => {
+				hideTips();
 				console.log(error);
+			});
 		});
-	});
+	} catch(error) {
+		hideTips();
+		console.log(error);
+	}
 }
 
 function createPopup(anchor, comments) {
-	anchor.title = "";
+	anchor.title = ""; // disable tooltips
 	anchor.querySelectorAll('*').forEach(child => {
-		child.title = "";
+		child.title = ""; // even spans can have titles
 	});
+
 	const popup = document.createElement("tooltip");
 	popup.innerHTML = comments;
 	Object.assign(popup.style, TIPSTYLE);
@@ -121,19 +139,17 @@ function createPopup(anchor, comments) {
 	popup.style.maxWidth = (fullW * MAXWIDTHR) + 'px';
 	popup.style.maxHeight = (fullH * MAXHEIGHTR) + 'px';
 
-	//popup.onmouseleave = function(){hideTips();};
 	popup.onclick = function(){hideTips();};
 	hideTips();
 	anchor.appendChild(popup);
-	//anchor.onmouseleave = function(){hideTips();};
 	cause = anchor;
 
 	timeout = setTimeout(function(popup){
-		showTips(popup);
+		showTip(popup);
 	}, POPUPDELAY, popup);
 }
 
-function showTips(popup) {
+function showTip(popup) {
 	const fullW = document.documentElement.clientWidth;
 	const fullH = document.documentElement.clientHeight;
 	const popW = popup.offsetWidth;
@@ -154,10 +170,13 @@ function hideTips() {
 	}
 }
 
+// the only event reliable enough to hide popups
+// others are not useful when mouse moves fast
 function mouseMoveListener(event) {
 	const elem = document.elementFromPoint(event.clientX, event.clientY);
-	if (cause && !cause.contains(elem))
+	if (cause && !cause.contains(elem)) // elem can be null
 		hideTips();
+
 	mouseX = event.clientX + OFFSETX;
 	mouseY = event.clientY + OFFSETY;
 }
@@ -174,6 +193,7 @@ function keyUpListener(event) {
 		pressed = false;
 }
 
+// set {useCapture: true} to detect all anchors with the single listener
 document.addEventListener("mouseenter", mouseEnterListener, true);
 document.addEventListener("mousemove", mouseMoveListener);
 document.addEventListener("keydown", keyDownListener);
