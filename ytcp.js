0bc4c041733d0a208fd4dd06621b57cbc6439fc4
Tamotsu Takahashi <ttakah+github@gmail.com>
2024-03-23 18:11:19 +0900

    Revert "Limit url to /entry/s/"
    3617953d24018ea1dffeaa3521fbde7b970803f7

--- ytcp.js
+++ ytcp.js
@@ -61,7 +61,7 @@
 	}
 
 	function enc(url) {
-		if (url?.match(/^https?:\/\/b\.hatena\.ne\.jp\/entry\/s\//)) {
+		if (url?.match(/^https?:\/\/b\.hatena\.ne\.jp\/entry\//)) {
 			return btoa(url)
 				.replace(/=/g, "")
 				.replace(/\+/g, "-")
