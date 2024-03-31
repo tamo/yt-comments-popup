789908c9ff5f7f802c9fb6b7919421e2eb059b34
Tamotsu Takahashi <ttakah+github@gmail.com>
2024-03-31 17:21:16 +0900

    Optimize by skipping same elem

--- ytcp.js
+++ ytcp.js
@@ -37,6 +37,7 @@
 	const cache = {};
 	let timeout = undefined;
 	let shown = undefined;
+	let lastelem = undefined;
 	let pressed = false;
 	let mouseX = 0;
 	let mouseY = 0;
@@ -301,6 +302,8 @@
 		mouseY = event.clientY;
 
 		const elem = document.elementFromPoint(mouseX, mouseY);
+		if (elem == lastelem) return;
+		lastelem = elem;
 		dM.groupCollapsed("mousemove");
 		dM.log(event, "element", elem);
 		if (!elem) {
